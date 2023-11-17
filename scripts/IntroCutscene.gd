9794af56ae2447e99a9b0d6fead56d33a3d0cdb1
GooDonyaRed <146790157+GooDonyaRed@users.noreply.github.com>
2023-11-17 20:04:05 +1100

    Add Intro Cinematic
    - Add new godot scene to project "IntroCutscene.gd"
    - Add functional intro cutscene that transitions into main scene
    
    To test, press F6 with IntroCutscene scene open in Godot editor

---
+++ scripts/IntroCutscene.gd
@@ -0,0 +1,76 @@
+extends Node2D
+
+@onready var pt1 = $Page1/AnimationPlayer
+@onready var pt2 = $Page2/AnimationPlayer
+@onready var pt3 = $Page3/AnimationPlayer
+@onready var pt4 = $Page4/AnimationPlayer
+@onready var pt5 = $Page5/AnimationPlayer
+@onready var pt6 = $Page6/AnimationPlayer
+@onready var pt7 = $Page7/AnimationPlayer
+
+var PageNum = 0 
+var eggs = 0
+
+func _ready():
+	$Page1.hide()
+	$Page2.hide()
+	$Page3.hide()
+	$Page4.hide()
+	$Page5.hide()
+	$Page6.hide()
+	$Page7.hide()
+	$Timer.start()
+	$TransitionTimer.start()
+	pass
+
+func _physics_process(delta):
+	
+	#print(str(PageNum))
+	
+	if PageNum == 0:
+		$Page1.show()
+		pt1.play("page_1")
+		PageNum = PageNum + 1
+		$Timer.start()
+	
+	elif PageNum == 1 and $Timer.time_left == 0:
+		$Page2.show()
+		pt2.play("page_2")
+		PageNum = PageNum + 1
+		$Timer.start()
+	
+	elif PageNum == 2 and $Timer.time_left == 0:
+		$Page3.show()
+		pt3.play("page_3")
+		PageNum = PageNum + 1
+		$Timer.start()
+	
+	elif PageNum == 3 and $Timer.time_left == 0:
+		$Page4.show()
+		pt4.play("page_4")
+		PageNum = PageNum + 1
+		$Timer.start()
+	
+	elif PageNum == 4 and $Timer.time_left == 0:
+		$Page5.show()
+		pt5.play("page_5")
+		PageNum = PageNum + 1
+		$Timer.start()
+	
+	elif PageNum == 5 and $Timer.time_left == 0:
+		$Page6.show()
+		pt6.play("page_6")
+		PageNum = PageNum + 1
+		$Timer.start()
+	
+	elif PageNum == 6 and $Timer.time_left == 0:
+		$Page7.show()
+		pt6.stop()
+		pt7.play("page_7")
+		PageNum = PageNum + 1
+		$Timer.start()
+
+	else:
+		if PageNum == 7 and $Timer.time_left == 0 and $TransitionTimer.time_left == 0:
+			print("ASS")
+			get_tree().change_scene_to_file("res://scenes/platformer_world.tscn")
