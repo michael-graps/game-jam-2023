cbe5b93c7489fa560bb50809e2cb8c5b7f2edacf
MeekaMew <jennifer.cara220@gmail.com>
2023-11-29 10:54:56 +1100

    Add Menu Buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,7 +6,7 @@ func _ready():
 	Engine.max_fps = 60
 
 func _on_intro_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn")
+	pass
 
 func _on_quick_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/area1_basement.tscn")
@@ -16,3 +16,7 @@ func _on_options_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn")
