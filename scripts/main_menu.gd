40d82e03528680bb3503466ad9698733d4e99f21
GooDonyaRed <146790157+GooDonyaRed@users.noreply.github.com>
2023-11-29 11:29:11 +1100

    Work on Main menu, rename button assets

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,22 +1,18 @@
 extends Control
 
 func _ready():
-	$VBoxContainer/IntroStart.grab_focus()
+	$play_button.grab_focus()
 	PlayerCollectionsTracker.resetcollections()
 	Engine.max_fps = 60
 
-func _on_intro_start_pressed():
-	pass
 
-func _on_quick_start_pressed():
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn")
+
+
+func _on_quickstart_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/area1_basement.tscn")
 
-func _on_options_button_pressed():
-	print("THIS BUTTON DOESN'T FUCKIN' DO ANYTHING RIGHT NOW, FUCK OFF")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
-
-
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/intro_cutscene.tscn")
